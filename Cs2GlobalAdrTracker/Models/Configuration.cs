7cb8be6404f5ef585cebff94a04bdae8014e3b44
xXCleanXx <erdwegsebastian@hotmail.com>
2023-10-03 23:20:36 +0200

    ~ WindowLocation class » readonly struct

--- Cs2GlobalAdrTracker/Models/Configuration.cs
+++ Cs2GlobalAdrTracker/Models/Configuration.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json.Serialization;
-using YetAnotherMonitor.Models;
 
 namespace Cs2GlobalAdrTracker.Models
 {
--- Cs2GlobalAdrTracker/Models/WindowLocation.cs
+++ Cs2GlobalAdrTracker/Models/WindowLocation.cs
@@ -1,8 +1,34 @@
-﻿namespace YetAnotherMonitor.Models
+﻿namespace Cs2GlobalAdrTracker.Models
 {
-    internal class WindowLocation
+    internal readonly struct WindowLocation
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        internal int X { get; }
+        internal int Y { get; }
+
+        internal WindowLocation(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static bool operator ==(WindowLocation windowLocation1, WindowLocation windowLocation2)
+        {
+            return windowLocation1.X == windowLocation2.X && windowLocation1.Y == windowLocation2.Y;
+        }
+
+        public static bool operator !=(WindowLocation windowLocation1, WindowLocation windowLocation2)
+        {
+            return !(windowLocation1 == windowLocation2);
+        }
+
+        public override readonly bool Equals(object obj)
+        {
+            return obj is WindowLocation wl && this == wl;
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return this.X.GetHashCode() ^ this.Y.GetHashCode();
+        }
     }
 }
